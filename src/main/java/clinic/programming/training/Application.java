03368c9f96152a0a8ffef5412f6efcf79e45f81e
frazu <fzuchows@gmail.com>
2017-06-10 11:41:53 +0200

    Adding application source file and .gitignore update

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,17 @@
+package clinic.programming.training;
+
+/**
+ * Created by FrankZ on 10.06.2017.
+ */
+public class Application {
+
+    public Application() {
+        System.out.println("Inside application");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Starting Application");
+        Application app = new Application();
+    }
+
+}
