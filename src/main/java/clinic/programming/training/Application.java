116b3e470ec4ced4a86dfb2fc70b258469349bd5
frazu <fzuchows@gmail.com>
2017-06-10 12:41:37 +0200

    Change Applciation with greet method and set JDK version as source/target in pom

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,5 +1,8 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by FrankZ on 10.06.2017.
  */
@@ -9,9 +12,19 @@ public class Application {
         System.out.println("Inside application");
     }
 
+
+    public void greet() {
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hello");
+        for(String greeting : greetings) {
+            System.out.println("Greeting: " + greeting);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Starting Application");
         Application app = new Application();
+        app.greet();
     }
 
 }
